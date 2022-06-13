0bd21ab3f7da7cf3b3a8cdb927a09e80b4885ebb
PavelPeacow <68381498+PavelPeacow@users.noreply.github.com>
2022-06-13 15:14:35 +0500

    added HabitLabel View, preparation to integrate CoreData

---
+++ HabbitTracker/SmallView/HabitLabel.swift
@@ -0,0 +1,75 @@
+//
+//  HabitLabel.swift
+//  HabbitTracker
+//
+//  Created by Павел Кай on 13.06.2022.
+//
+
+import SwiftUI
+
+struct HabitLabel: View {
+    @Binding var habitItem: HabbitItem
+    
+    var fetchCurrentWeek: [Date] {
+        
+        let calendar = Calendar.current
+        let week = calendar.dateInterval(of: .weekOfMonth, for: Date())
+        var currentWeek: [Date] = []
+        
+        
+        (1...7).forEach { day in
+            if let weekday = calendar.date(byAdding: .day, value: day, to: week!.start) {
+                currentWeek.append(weekday)
+            }
+        }
+        
+        return currentWeek
+    }
+    
+    func extractDate(date: Date, format: String) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = format
+        return formatter.string(from: date)
+    }
+    
+    
+    var body: some View {
+        List {
+            VStack {
+                HStack {
+                    ForEach(fetchCurrentWeek, id: \.self) { day in
+                        
+                        VStack {
+                            Text(extractDate(date: day, format: "dd"))
+                            
+                            Text(extractDate(date: day, format: "EEE"))
+                        }
+                        .frame(maxWidth: .infinity)
+                    }
+                }
+                
+                Divider()
+                
+                HStack {
+                    HStack{
+                        Circle()
+                            .fill(.red)
+                            .frame(width: 30, height: 30)
+                        Text(habitItem.nameHabbit)
+                        Spacer()
+                    }
+                    HStack {
+                        Text("\(habitItem.streak)")
+                    }
+                }
+            }
+        }
+        
+    }
+}
+
+//struct HabitLabel_Previews: PreviewProvider {
+//    static var previews: some View {
+//        HabitLabel(habitItem: <#Binding<Habbits>#>)
+//    }
+//}
