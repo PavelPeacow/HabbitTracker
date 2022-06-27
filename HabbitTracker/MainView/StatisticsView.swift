59f839ade5caa8896e13d43f1970aee719f1f6ef
PavelPeacow <68381498+PavelPeacow@users.noreply.github.com>
2022-06-27 19:32:27 +0500

    Updated UIViewRepresentable(added class Coordinator), some testing

--- HabbitTracker/MainView/StatisticsView.swift
+++ HabbitTracker/MainView/StatisticsView.swift
@@ -10,17 +10,19 @@ import UIKit
 import FSCalendar
 
 struct StatisticsView: View {
+    @State private var selectedDay = Date()
+    
     var body: some View {
         VStack {
             Text("Statistic")
-            CalendarView()
+            CalendarView(selectedDay: $selectedDay)
                 .padding()
                 .background(
                     RoundedRectangle(cornerRadius: 30)
                         .foregroundColor(.orange)
                 )
                 .frame(height: 260)
-            
+            Text("\(selectedDay)")
             Spacer()
             
             VStack {
@@ -52,16 +54,38 @@ struct StatisticsView_Previews: PreviewProvider {
 }
 
 
-
 struct CalendarView: UIViewRepresentable {
+    @Binding var selectedDay: Date
     var calendar = FSCalendar()
     
-    func makeUIView(context: Context) -> UIView {
-        calendar.backgroundColor = .orange
-        return FSCalendar(frame: CGRect(x: 0.0, y: 40.0, width: 200, height: 200))
+    func updateUIView(_ uiView: FSCalendar, context: Context) {
+    }
+    
+
+    //MARK: Make UI
+    func makeUIView(context: Context) -> FSCalendar {
+        calendar.delegate = context.coordinator
+        calendar.dataSource = context.coordinator
+    
+        return calendar
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, FSCalendarDelegate, FSCalendarDataSource {
+        var parent: CalendarView
+
+        init(_ parent: CalendarView) {
+            self.parent = parent
+        }
+        
+        func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+            parent.selectedDay = date
+        }
         
     }
     
-    func updateUIView(_ uiView: UIView, context: Context) {
-    }
+
 }
