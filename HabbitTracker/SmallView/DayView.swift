8b78e7391eaf4b80329780d38f084948aed63f4e
PavelPeacow <68381498+PavelPeacow@users.noreply.github.com>
2022-07-05 14:31:14 +0500

    moved currentWeek ui component from HabitLabelView to new CurrentWeekView

---
+++ HabbitTracker/SmallView/DayView.swift
@@ -0,0 +1,20 @@
+//
+//  DayView.swift
+//  HabbitTracker
+//
+//  Created by Павел Кай on 05.07.2022.
+//
+
+import SwiftUI
+
+struct DayView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DayView_Previews: PreviewProvider {
+    static var previews: some View {
+        DayView()
+    }
+}
