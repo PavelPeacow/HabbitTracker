e7fd329b0997553a31ea0e3980b159f749f2dc99
Pavel Kai <68381498+PavelPeacow@users.noreply.github.com>
2022-07-09 20:29:40 +0500

    Revert "fixed bugs, now onTapGesture allows tap only selectedDays"
    This reverts commit 7fb888a161e760ad26bed2eba8b81a1f8a4f23b6.

--- HabbitTracker/SmallView/HabitLabel.swift
+++ HabbitTracker/SmallView/HabitLabel.swift
@@ -29,16 +29,13 @@ struct HabitLabel: View {
                     .background(
                         ZStack() {
                             Capsule()
-                            //                                .fill(habitViewModel.isToday(date: dayDate) ? .black : Color(habitItem.color ?? "Color-1"))
-                                .fill(habitViewModel.showSelectedDays(frequency: habitItem.frequency ?? []).contains(dayNum) ? Color(habitItem.color ?? "Color-1") : .gray)
-                                .opacity(habitViewModel.showSelectedDays(frequency: habitItem.frequency ?? []).contains(dayNum) ? 1.0 : 0.5)
+//                                .fill(habitViewModel.isToday(date: dayDate) ? .black : Color(habitItem.color ?? "Color-1"))
+                                .fill(habitViewModel.showSelectedDays(frequency: habitItem.frequency ?? []).contains(dayNum) ? .orange : .red)
                                 .frame(width: 40, height: 55)
                                 .onTapGesture {
-                                    if habitViewModel.showSelectedDays(frequency: habitItem.frequency ?? []).contains(dayNum) {
-                                        habitViewModel.dayComplete(context: moc, habitToSave: habitItem)
-                                    }
+                                    habitViewModel.dayComplete(context: moc, habitToSave: habitItem)
+                                    habitItem.streak += 1 - 1 //dunno why += 1 equals 2
                                 }
-                            
                         }
                     )
                 }
