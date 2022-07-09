60737d7ebddbe25018478484242e7a6d0adc1995
Pavel Kai <68381498+PavelPeacow@users.noreply.github.com>
2022-07-09 20:29:40 +0500

    Revert "Updated UIViewRepresentable(added class Coordinator), some testing"
    This reverts commit a3669147f211174beef21aaf2f4b0466881439de.

--- HabbitTracker/MainView/StatisticsView.swift
+++ HabbitTracker/MainView/StatisticsView.swift
@@ -10,19 +10,17 @@ import UIKit
 import FSCalendar
 
 struct StatisticsView: View {
-    @State private var selectedDay = Date()
-    
     var body: some View {
         VStack {
             Text("Statistic")
-            CalendarView(selectedDay: $selectedDay)
+            CalendarView()
                 .padding()
                 .background(
                     RoundedRectangle(cornerRadius: 30)
                         .foregroundColor(.orange)
                 )
                 .frame(height: 260)
-            Text("\(selectedDay)")
+            
             Spacer()
             
             VStack {
@@ -54,38 +52,16 @@ struct StatisticsView_Previews: PreviewProvider {
 }
 
 
+
 struct CalendarView: UIViewRepresentable {
-    @Binding var selectedDay: Date
     var calendar = FSCalendar()
     
-    func updateUIView(_ uiView: FSCalendar, context: Context) {
-    }
-    
-
-    //MARK: Make UI
-    func makeUIView(context: Context) -> FSCalendar {
-        calendar.delegate = context.coordinator
-        calendar.dataSource = context.coordinator
-    
-        return calendar
-    }
-    
-    func makeCoordinator() -> Coordinator {
-        Coordinator(self)
-    }
-
-    class Coordinator: NSObject, FSCalendarDelegate, FSCalendarDataSource {
-        var parent: CalendarView
-
-        init(_ parent: CalendarView) {
-            self.parent = parent
-        }
-        
-        func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
-            parent.selectedDay = date
-        }
+    func makeUIView(context: Context) -> UIView {
+        calendar.backgroundColor = .orange
+        return FSCalendar(frame: CGRect(x: 0.0, y: 40.0, width: 200, height: 200))
         
     }
     
-
+    func updateUIView(_ uiView: UIView, context: Context) {
+    }
 }
